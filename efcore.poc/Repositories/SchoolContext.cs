3351322caed4c08049d014aeebcedd6550c8d3d4
gmichalopulos <gregomicha@gmail.com>
2020-10-02 23:02:08 -0300

    Adding SchoolContext

---
+++ efcore.poc/Repositories/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using efcore.poc.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace efcore.poc.Repositories
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext([NotNull] DbContextOptions<SchoolContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
